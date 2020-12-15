2658dbdc7ca1f5eeb09b4ac5b0f69650305247d4
Nathan Smith <64522880+Psyonic123@users.noreply.github.com>
2020-12-15 13:40:26 -0600

    Add files via upload

---
+++ Dungeon Jewler/2447e855-af0f-45db-a034-b9378b702008/Assets/Scripts/NodePiece.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.UI;
+
+public class NodePiece : MonoBehaviour
+{
+    public int value;
+    public Point index;
+
+    [HideInInspector]
+    public Vector2 pos;
+    [HideInInspector]
+    public RectTransform rect;
+
+    Image img;
+
+    public void Initialize(int v, Point p, Sprite piece)
+    {
+        img = GetComponent<Image>();
+        rect = GetComponent<RectTransform>();
+
+        value = v;
+        SetIndex(p);
+        img.sprite = piece;
+    }
+
+    public void SetIndex(Point p)
+    {
+        index = p;
+        ResetPosition();
+        UpdateName();
+    }
+    public void ResetPosition()
+    {
+        pos = new Vector2(32 + (78 * index.x), -32 - (80 * index.y));
+    }
+    void UpdateName()
+    {
+        transform.name = "Node [" + index.x + ", " + index.y + "]";
+    }
+}
